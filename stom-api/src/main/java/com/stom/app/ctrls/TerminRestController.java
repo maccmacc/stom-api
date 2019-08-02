2a53a47925ab38b6ef20d411c43f67ffea6f4c46
djordjeprzulj <przulj@gmail.com>
2019-08-02 22:40:12 +0200

    Termin rest controller fix

--- stom-api/src/main/java/com/stom/app/ctrls/TerminRestController.java
+++ stom-api/src/main/java/com/stom/app/ctrls/TerminRestController.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +22,7 @@ import com.stom.app.reps.TerminRepository;
 
 @RestController
 @CrossOrigin
+@RequestMapping("termin")
 public class TerminRestController {
 	
 	@Autowired
@@ -26,8 +31,7 @@ public class TerminRestController {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	@RequestMapping(value = "terminNextId", method = RequestMethod.GET)
-	public int getNextId() {		
+	private int getNextId() {		
         Integer sledeci = jdbcTemplate.queryForObject("SELECT max(id)+1 "
 													+ "FROM termin", Integer.class);
         if (sledeci == null)
@@ -35,28 +39,25 @@ public class TerminRestController {
         return sledeci.intValue();
 	}
 	
-	@RequestMapping(value = "termin", method = RequestMethod.GET)
+	@GetMapping
 	public Collection<Termin> getAll() {
 		return terminRepository.findAll();
-		
 	}
 	
-	@RequestMapping(value = "termin/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/{id}")
     public ResponseEntity<Termin> get(@PathVariable("id") int id) {
 		Optional<Termin> obj = terminRepository.findById(id);
-        return new ResponseEntity(obj, HttpStatus.OK);
+        return new ResponseEntity(obj.get(), HttpStatus.OK);
     }
 	
-	@RequestMapping(value = "termin", method = RequestMethod.POST)
+	@PostMapping
     public ResponseEntity<Void> insertTermin(@RequestBody Termin obj) {
-        if (terminRepository.existsById(obj.getId())) {
-            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-        }		
+		obj.setId(this.getNextId());
 		terminRepository.save(obj);
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 	
-	@RequestMapping(value = "termin/{id}", method = RequestMethod.PUT)
+	@PutMapping(value = "/{id}")
     public ResponseEntity<Void> updateTermin(@PathVariable("id") Integer id, @RequestBody Termin obj) {
         if (!terminRepository.existsById(id)) {
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
@@ -65,7 +66,7 @@ public class TerminRestController {
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 	
-	@RequestMapping(value = "termin/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteTermin(@PathVariable("id") Integer id) {
         if (!terminRepository.existsById(id)) {
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
