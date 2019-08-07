acaced5d2bda0bb53560ce717a75d868f46c8c38
djordjeprzulj <przulj@gmail.com>
2019-08-07 19:03:52 +0200

    swagger config

--- stom-api/src/main/java/com/stom/app/config/SpringSecConfig.java
+++ stom-api/src/main/java/com/stom/app/config/SpringSecConfig.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -59,6 +59,16 @@ public class SpringSecConfig extends WebSecurityConfigurerAdapter {
         authenticationManagerBuilder.authenticationProvider(authenticationProvider);
     }
     
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources",
+                                   "/configuration/security",
+                                   "/swagger-ui.html",
+                                   "/webjars/**",
+                                   "/csrf");
+    }
     
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {       
@@ -73,12 +83,13 @@ public class SpringSecConfig extends WebSecurityConfigurerAdapter {
             	.antMatchers("/", "/api/auth/login",
             		"/api/users/register",
             		"/api/auth/logout",
-            		"/api/v2/**",
-            		"/api/configuration/ui",
-            		"/api/swagger-resources/**",
-            		"/api/configuration/security",
-            		"/api/swagger-ui.html",
-            		"/api/webjars/**").permitAll()
+            		"/v2/**",
+            		"/configuration/ui",
+            		"/swagger-resources/**",
+            		"/configuration/security",
+            		"/swagger-ui.html",
+            		"/webjars/**",
+            		"/csrf").permitAll()
         	.anyRequest().authenticated();
 		
         httpSecurity.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
